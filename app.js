2429c84a7cb3d0bc9b388e4869d1ba17ab5fb74e
Allen An <zifeng.allen@gmail.com>
2020-08-07 15:29:09 -0400

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require('cors');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
@@ -17,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use('/static/',express.static(path.join(__dirname, 'public')));
 
 
